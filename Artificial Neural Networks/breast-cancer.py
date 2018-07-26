5674da9e599b88d793a69685957d3ad26dcc1034
Henrique Alves <henri.falves@hotmail.com>
2018-07-26 05:22:05 -0300

    Código funcional sobre uma base de cancer de mama com sklearn

---
+++ Artificial Neural Networks/breast-cancer.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Sep 19 20:38:44 2017
+
+@author: Jones
+"""
+import numpy as np
+from sklearn import datasets
+from sklearn.neural_network import MLPClassifier
+
+base = datasets.load_breast_cancer()
+
+neuralNetwork = MLPClassifier( 
+    max_iter=1000,
+    activation='logistic', 
+    learning_rate_init=0.001
+)
+neuralNetwork.fit(base.data, base.target)
+print(neuralNetwork.predict(np.array([base.data[0]])))
+print(neuralNetwork.predict(np.array([base.data[20]])))
